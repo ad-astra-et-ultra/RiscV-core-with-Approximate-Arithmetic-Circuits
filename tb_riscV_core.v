aa29e44eb750186087ba384e24759995cadb4541
ABHISHEK ANAND <92263062+shake-coder@users.noreply.github.com>
2023-02-13 01:13:46 +0530

    Create tb_riscV_core.v

---
+++ tb_riscV_core.v
@@ -0,0 +1,21 @@
+module top();
+  reg clk;
+  reg reset;
+  
+  initial begin
+    reset<=1;
+    clk<=0;
+  end
+  
+  always #10 clk <= ~clk;
+  
+  cpu_top cpu_inst(.clk(clk), .reset(reset));
+
+  initial begin
+    #1 reset = ~reset;
+  end
+
+  initial
+   #60000 $finish;
+  
+endmodule
